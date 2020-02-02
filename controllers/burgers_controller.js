68d1ad8c5091f54b4353ad4676e91ef226e4a811
jgohbb <55856950+jgohbb@users.noreply.github.com>
2020-02-02 03:11:32 -0700

    All working nicely except burger name no show

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,8 +14,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res){
-    burger.insertOne(["burger_name", " devoured"], [req.body.burger_name, req.body.devoured], function (result) {
-        res.json(result);
+    burger.insertOne(["burger_name", " devoured"], 
+        [req.body.burger_name, req.body.devoured], function (result) {
+    
+    res.json(result);
     })
 });
 
