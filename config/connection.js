b49b5ebe2ae80295f1ddbcafe68c40596f09f596
jgohbb <55856950+jgohbb@users.noreply.github.com>
2020-02-03 23:50:57 -0700

    added password protect with env

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Aiglue2019",
-  database: "burgers_db"
-});
+require("dotenv").config();
 
+var data = process.env;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: data.password,
+    database: "burgers_db"
+  });
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
