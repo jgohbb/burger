9c0053555726dd49e43922348eddcc753b44549b
jgohbb <55856950+jgohbb@users.noreply.github.com>
2020-02-04 21:29:12 -0700

    deploy heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO burgers (burger_name, devoured)
-VALUES 
-("Bacon and Cheese Burger", false), 
+VALUES ("Bacon and Cheese Burger", false), 
 ("Swiss Mushroom Burger", false), 
 ("Astro Burger", false);
 
